10ae538b76b93d1afb5f6c31c0236f608014808b
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-16 15:19:26 +0200

    init git repo

---
+++ Front/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <header> 
+    <div class="header-flex"> 
+      <img src="./images/Logo_red.png" alt="Groupomania logo">
+      <nav class="nav-flex">
+        <a href=""> Se connecter </a> 
+        <a href=""> S'inscrire </a>
+      </nav>
+    </div>
+  </header>
+
+  <main> 
+    <section class="main-flex">
+      <div class="illustration-signup"> 
+          <img src="./images/Illustration-sign-up.png" alt="Illustration Sign-Up">
+
+
+      </div>
+      
+
+
+
+    </section>
+
+
+  </main>
+
+
+</template>
+
+<style></style>
+
+
