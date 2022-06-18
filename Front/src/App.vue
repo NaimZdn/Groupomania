1a5d5c28225fec796645b7104747bf6bc553ecbd
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-19 01:21:42 +0200

    add man page + make header

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -3,79 +3,23 @@
 </script>
 
 <template>
-<body>
-  <header class="container"> 
-    <div class="header-flex-login"> 
-      <div class="logo-flex">
+
+  <header class="bg-main">
+    <div class="header-flex-main"> 
+      <div class="logo-flex-main">
       <img src="./images/Logo_red.png" alt="Groupomania logo">
       </div>
-      <nav class="nav-flex-login">
-        <a href=""> Se connecter </a> 
-        <a href=""> S'inscrire </a>
+      <nav class="nav-flex-main">
+        <p> Nouvelle publication </p>
+        <a href=""> <img class="user-picture" src="./images/Photo CV.jpg"> </a>
       </nav>
+
+
     </div>
   </header>
 
-  <main class="container" > 
-    <section class="main-flex-login">
-      <div class="illustration-login"> 
-          <img src="./images/Illustration-login.png" alt="Illustration Login">
-      </div> 
-      
-      <div class="form-signup"> 
-        <h1> S'identifier </h1>
-        
-           <div class="input-icon"> 
-            <input type="mail" placeholder="Email"> 
-            <img class="icon-envelope-login" src="./images/Icon-envelope.png"> 
-          </div>
+ 
 
-          <div class="input-icon"> 
-            <input type ='password' placeholder="Mot de passe" > 
-            <img class="icon-key-login" src="./images/Icon-key.png"> 
-          </div>
-
-          
-
-          <div class="button-sign-login">  
-            <button> Se connecter </button>
-          </div>
-
-      </div>
-    </section>
-
-
-  </main>
-
-  <footer class="container footer-flex"> 
-    <div class="footer-icon">
-      <a href="">
-        <img class="icon" src="./images/Icon-facebook.png">
-      </a>
-    </div> 
-
-    <div class="footer-icon">
-      <a href="">
-        <img class="icon" src="./images/Icon-linkedin.png">
-      </a>
-    </div> 
-
-    <div class="footer-icon">
-      <a href="">
-        <img class="icon" src="./images/Icon-twitter.png">
-      </a>
-    </div> 
-
-    <div class="footer-icon">
-      <a href="">
-        <img class="icon" src="./images/Icon-mail.png">
-      </a>
-    </div> 
-
-
-  </footer>
-
-  </body>
 
 </template>
 
