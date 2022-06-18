fa35afdc6cd42584629befcecee3a7f98f57a0e8
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-19 00:36:56 +0200

    add login page

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+
 /*import BootstrapVue3 from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
--- Front/src/sign-up.vue
+++ Front/src/sign-up.vue
@@ -5,11 +5,11 @@
 <template>
 <body>
   <header class="container"> 
-    <div class="header-flex-login"> 
+    <div class="header-flex"> 
       <div class="logo-flex">
       <img src="./images/Logo_red.png" alt="Groupomania logo">
       </div>
-      <nav class="nav-flex-login">
+      <nav class="nav-flex">
         <a href=""> Se connecter </a> 
         <a href=""> S'inscrire </a>
       </nav>
@@ -17,28 +17,35 @@
   </header>
 
   <main class="container" > 
-    <section class="main-flex-login">
-      <div class="illustration-login"> 
-          <img src="./images/Illustration-login.png" alt="Illustration Login">
+    <section class="main-flex">
+      <div class="illustration-signup"> 
+          <img src="./images/Illustration-sign-up.png" alt="Illustration Sign-Up">
       </div> 
       
       <div class="form-signup"> 
-        <h1> S'identifier </h1>
-        
+        <h1> Créez votre compte</h1>
+          <div class="input-icon"> 
+            <input type="pseudo" placeholder="Pseudo">  
+            <img class="icon-pseudo" src="./images/Icon-user.png"> 
+          </div>
+
            <div class="input-icon"> 
             <input type="mail" placeholder="Email"> 
-            <img class="icon-envelope-login" src="./images/Icon-envelope.png"> 
+            <img class="icon-envelope" src="./images/Icon-envelope.png"> 
           </div>
 
           <div class="input-icon"> 
             <input type ='password' placeholder="Mot de passe" > 
-            <img class="icon-key-login" src="./images/Icon-key.png"> 
+            <img class="icon-key" src="./images/Icon-key.png"> 
           </div>
 
-          
+          <div class="checkbox-cgu"> 
+            <input type="checkbox">
+            <span class="cgu"> J'accepte les <a href="#">conditions générales d'utilisations</a></span>
+          </div>
 
-          <div class="button-sign-login">  
-            <button> Se connecter </button>
+          <div class="button-sign">  
+            <button> Créer mon compte </button>
           </div>
 
       </div>
