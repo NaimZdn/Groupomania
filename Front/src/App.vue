5145b1d389950181aeaf0e1a0dd1c7981b8434da
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-06-20 16:52:24 +0200

    finish post page

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -13,10 +13,44 @@
         <p> Nouvelle publication </p>
         <a href=""> <img class="user-picture" src="./images/Photo CV.jpg"> </a>
       </nav>
-
-
     </div>
   </header>
+
+  <main class="container"> 
+    <div class="post">
+      <div class="post-header-flex"> 
+        <div class="post-picture">
+          <a href=""> <img class="user-post-picture" src="./images/Photo CV.jpg"> </a>
+        </div>
+
+        <div class="post-text">
+          <h2>Pseudo</h2>
+          <p> 14 juin 2022 à 15h00</p>
+        </div>
+        <div class="post-option"> 
+          <img src="./images/Icon-option.png">
+        </div>
+        </div>   
+    
+      <div class="post-image"> 
+        <img src="./images/Geometric abstract-02.jpg">
+      </div>
+
+      <div class="post-footer-flex">
+        <div class="post-comment">
+          <img src="./images/Icon-chat.png">
+          <span class="post-comment-text"> 7 commentaires </span>
+        </div>
+
+        <div class="post-like"> 
+           <i class="fa-regular fa-heart"></i>
+           <i class="fa-solid fa-heart"></i>
+          <span class="like-number"> 5 </span>
+        </div>
+      </div>
+    </div>
+  </main>
+
 
  
 
