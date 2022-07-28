245ae8a238eb43015c60b6e6e467aaf63f5d7353
Naïm Zidouni <naim.zidouni@outloook.fr>
2022-07-28 14:27:26 +0200

    add password validator

--- Back/routes/user-router.js
+++ Back/routes/user-router.js
@@ -4,14 +4,15 @@ const router = express.Router();
 const userCtrl = require('../controllers/user-controllers');
 const auth = require('../middlewares/auth'); 
 const multer = require('../middlewares/multer-config'); 
+const passwordValidator = require('../middlewares/password-validator');
 
 router.post('/login', userCtrl.login); 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidator, userCtrl.signup);
 router.get('/logout', auth, userCtrl.logout); 
 
 router.get('/user/:id', auth, userCtrl.getOneUser);
 router.get('/users',  auth, userCtrl.getAllUsers);
-router.put('/user/:id', multer,userCtrl.updateProfil);
+// router.put('/user/:id', multer, userCtrl.updateProfil);
 router.delete('/:id/delete', userCtrl.deleteAccount);
 
 
